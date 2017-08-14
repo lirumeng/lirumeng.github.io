103fa8bfeb8115b344eae2872ea64aeee39302bf
111 <a111@andongdeMac-mini.local>
2017-08-14 20:46:42 +0800

    update code

--- vue-learning/src/js/cart.js
+++ vue-learning/src/js/cart.js
@@ -1,25 +1,33 @@
-var vm = new Vue({
+new Vue({
   el: '#app',
   data: {
     totalMoney: 0,
     productList: []
   },
-  filters: function() {
-
+  filters:{
+    formatMoney: function(value){
+      return '￥'+ value.toFixed(2);
+    }
   },
   mounted: function() {
-    this.cartView();
+    this.$nextTick(function(){
+      this.cartView();   //入口方法
+    });
   },
   methods: {
     cartView: function() {
-      var _this = this;
-      _this.$http.get("src/data/cartData.json", {
+      // var _this = this;
+      let  _this = this;
+      this.$http.get("src/data/cartData.json", {
         id: 123
-      }).then(function(res) {
+      }).then(res=>{
         var data = res.data;
-        _this.productList = data.result.list;
-        _this.totalMoney = data.totalMoney;
+        this.productList = data.result.list;
+        this.totalMoney = data.totalMoney;
       })
     }
   }
 });
+Vue.filter("money", function(value, type){   //全局过滤器
+  return "￥" + value.toFixed(2) + type;
+})
---
+++ vue-learning/src/js/index.js
@@ -0,0 +1,2 @@
+// var Vue = require("vue");
+// var VueResource = require("vue-resource");
